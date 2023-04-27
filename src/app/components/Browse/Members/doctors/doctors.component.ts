d07b4ed365bc856187d62f51d1c3380627a55744
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-04-27 21:08:32 +0200

    Update doctors.component.ts

--- src/app/components/Browse/Members/doctors/doctors.component.ts
+++ src/app/components/Browse/Members/doctors/doctors.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 import {Person} from "../../../../entities/Person";
+import {PersonService} from "../../../../services/person.service";
+
 
 @Component({
   selector: 'app-doctors',
   templateUrl: './doctors.component.html',
   styleUrls: ['./doctors.component.css']
 })
-export class DoctorsComponent {
-  personList: Person[] = []
+export class DoctorsComponent implements OnInit{
 
-  constructor() {
+  personList: Array<Person> = new Array<Person>()
+  url: string[] = [];
+
+  constructor(private router: Router, private prs: PersonService) {}
+
+  ngOnInit(): void
+  {
+    this.prs.getStatus(true).then(data =>
+    {
+      this.personList = data!
+      for (let i = 0; i<this.personList.length; i++)
+        this.prs.getPhoto(this.personList[i].id).then(data => {if (data) this.url[i] = data})
+    })
   }
-
 }
