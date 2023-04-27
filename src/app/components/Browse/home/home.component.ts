60956c4b5dfadaf55b56519c9ff9ae8442f9a1f2
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-04-27 23:00:56 +0200

    Update home.component.ts

--- src/app/components/Browse/home/home.component.ts
+++ src/app/components/Browse/home/home.component.ts
@@ -4,6 +4,14 @@ import {News} from "../../../entities/News";
 import {NewsService} from "../../../services/news.service";
 import {HttpClient} from "@angular/common/http";
 import {LanguageService} from "../../../services/language.service";
+import { Article } from 'src/app/entities/Article';
+import { ArticleService } from 'src/app/services/article.service';
+import { Partner } from 'src/app/entities/Partner';
+import { PartnersService } from 'src/app/services/partners.service';
+import { Person } from 'src/app/entities/Person';
+import { PersonService } from 'src/app/services/person.service';
+import { Event } from 'src/app/entities/Event';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-home',
@@ -11,15 +19,25 @@ import {LanguageService} from "../../../services/language.service";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+  eventList : Event[] = new Array<Event>()
+  articleList : Article[] = new Array<Article>()
+  partnersList : Partner[] = new Array<Partner>()
+  personList : Person[] = new Array<Person>()
   newsList: News[] = new Array<News>()
   url: string[] = [];
   content: any;
 
-  constructor(private router: Router, private ns: NewsService,private ls: LanguageService) {}
+
+  constructor(private router: Router, private ns: NewsService,private ls: LanguageService , private ar : ArticleService , private prt : PartnersService , private prs : PersonService , private ev : EventService) {}
 
   async ngOnInit()
   {
+    await this.ev.getAll().then(async (data) => { if (data) this.eventList = data;})
+    await this.prs.getAll().then(async (data) => { if (data) this.personList = data;})
+    await this.ns.getAll().then(async (data) => { if (data) this.articleList = data;})
+    await this.prt.getAll().then(async (data) => { if (data) this.partnersList = data;})
     this.ls.getLanguage().subscribe(data => this.content=data)
+    
     await this.ns.getAll().then( async data =>
     {
       if (data)
@@ -28,6 +46,7 @@ export class HomeComponent implements OnInit{
         await this.ns.getPhoto(this.newsList[i].id).then(data => {if (data) this.url[i] = data})
     })
   }
+  
 
 
   viewNews(id: number|undefined) {
