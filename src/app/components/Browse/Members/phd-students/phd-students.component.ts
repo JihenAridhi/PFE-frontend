25f5be38bad558d1f6cfd03478a6a1cb8bac0e47
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-04-27 21:09:01 +0200

    Update phd-students.component.ts

--- src/app/components/Browse/Members/phd-students/phd-students.component.ts
+++ src/app/components/Browse/Members/phd-students/phd-students.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Person} from "../../../../entities/Person";
+import {PersonService} from "../../../../services/person.service";
 
 @Component({
   selector: 'app-phd-students',
   templateUrl: './phd-students.component.html',
   styleUrls: ['./phd-students.component.css']
 })
-export class PhdStudentsComponent {
+export class PhdStudentsComponent implements OnInit{
 
+  personList: Array<Person> = new Array<Person>()
+  url: string[] = [];
+
+  constructor(private router: Router, private prs: PersonService) {}
+
+  ngOnInit(): void
+  {
+    this.prs.getStatus(true).then(data =>
+    {
+      this.personList = data!
+      for (let i = 0; i<this.personList.length; i++)
+        this.prs.getPhoto(this.personList[i].id).then(data => {if (data) this.url[i] = data})
+    })
+  }
 }
