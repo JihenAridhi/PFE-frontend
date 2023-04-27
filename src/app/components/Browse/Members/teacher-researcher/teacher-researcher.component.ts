b7b141a1cf16d9c45a42e9d9c07e267a0a0876b7
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-04-27 21:09:49 +0200

    Update teacher-researcher.component.ts

--- src/app/components/Browse/Members/teacher-researcher/teacher-researcher.component.ts
+++ src/app/components/Browse/Members/teacher-researcher/teacher-researcher.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Person} from "../../../../entities/Person";
+import {PersonService} from "../../../../services/person.service";
 
 @Component({
   selector: 'app-teacher-researcher',
   templateUrl: './teacher-researcher.component.html',
   styleUrls: ['./teacher-researcher.component.css']
 })
-export class TeacherResearcherComponent {
+export class TeacherResearcherComponent implements OnInit{
+
+  personList: Array<Person> = new Array<Person>()
+  url: string[] = [];
+
+  constructor(private router: Router, private prs: PersonService) {}
+
+  ngOnInit(): void
+  {
+    this.prs.getStatus(true).then(data =>
+    {
+      this.personList = data!
+      for (let i = 0; i<this.personList.length; i++)
+        this.prs.getPhoto(this.personList[i].id).then(data => {if (data) this.url[i] = data})
+    })
+  }
 
 }
