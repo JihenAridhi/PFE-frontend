56fd92ef2dbd6171106a7f9af7f68a8cd025bd4f
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-05-02 11:11:11 +0200

    Update partners.component.ts

--- src/app/components/Browse/partners/partners.component.ts
+++ src/app/components/Browse/partners/partners.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Partner } from 'src/app/entities/Partner';
+import { PartnersService } from 'src/app/services/partners.service';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.css']
 })
-export class PartnersComponent {
+export class PartnersComponent implements OnInit
+{
+  partnersList: Array<Partner> = new Array<Partner>()
+  url: string[] = [];
+
+  constructor(private router: Router, private pr: PartnersService) {}
+  ngOnInit(): void
+  {
+
+    this.pr.getAll().then(data =>
+    {
+      if (data)
+        this.partnersList = data!
+        for (let i = 0; i<this.partnersList.length; i++)
+        this.pr.getPhoto(this.partnersList[i].id).then(data => {if (data) this.url[i] = data})
+      
+    })
+  }  
 
 }
