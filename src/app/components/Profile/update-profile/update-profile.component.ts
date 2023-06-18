974665774bea904b947655a1827e3d9d57165f50
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-18 23:04:02 +0100

    Update update-profile.component.ts

--- src/app/components/Profile/update-profile/update-profile.component.ts
+++ src/app/components/Profile/update-profile/update-profile.component.ts
@@ -74,6 +74,10 @@ export class UpdateProfileComponent implements OnInit{
 
   updatePass(updateP: NgForm) {
     console.log(this.person.password)
+    if (updateP.value.newP.length < 8 || !/\d/.test(updateP.value.newP) || !/[a-zA-Z]/.test(updateP.value.newP)) {
+      alert('Please enter a new password that is at least 8 characters long and contains both letters and numbers.');
+      return; // Exit the function early
+    }
     if(updateP.value.newP != updateP.value.confirmP)
       alert('please confirm your new password')
       else if (this.person.password != updateP.value.oldP)
